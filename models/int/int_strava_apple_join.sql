670f38ff8e334952f9ef6209ebcf497bc18aa067
Kevin Hannon Schulman <kevinhannon95@gmail.com>
2024-06-06 01:36:24 +0000

    simplified date formatting in stg and int models

--- models/int/int_strava_apple_join.sql
+++ models/int/int_strava_apple_join.sql
@@ -8,13 +8,13 @@ with hrv_sdnn as (
 
 , final as (
     select
-        distinct date(strava.workout_start_eastern) as workout_date
+        distinct strava.workout_date
         , strava.miles
         , strava.minutes
         , strava.elevation_gain_ft as elev_gain_ft
         , hrv_sdnn.daily_avg_hrv as daily_avg_hrv
     from strava
-    inner join hrv_sdnn on date(strava.workout_start_eastern) = date(hrv_sdnn.timestamp_eastern)
+    inner join hrv_sdnn on strava.workout_date = date(hrv_sdnn.timestamp_eastern)
     order by workout_date asc
 )
 
--- models/staging/bigquery/stg_Strava_Data.sql
+++ models/staging/bigquery/stg_Strava_Data.sql
@@ -4,11 +4,11 @@ with source as (
 
 , final as (
     select
-        format_timestamp('%F %T', start_date_local) as workout_start_eastern
+        date(start_date_local) as workout_date
         , (distance * .000621) as miles
         , (moving_time / 60) as minutes
         , (total_elevation_gain * 3.281) as elevation_gain_ft
     from source
 )
 
-select * from final order by workout_start_eastern desc
+select * from final order by workout_date desc
